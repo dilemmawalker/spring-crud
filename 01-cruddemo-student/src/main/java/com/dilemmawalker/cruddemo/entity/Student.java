b3e995773fdec3c64f7447cad02dd4fb5b4a2978
Yash Handa <yashhanda500@gmail.com>
2024-12-28 18:11:18 +0530

    added table & columns using JPA(Hibernate)

---
+++ 01-cruddemo-student/src/main/java/com/dilemmawalker/cruddemo/entity/Student.java
@@ -0,0 +1,56 @@
+package com.dilemmawalker.cruddemo.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Table(name="student")
+@Getter
+@Setter
+public class Student {
+
+    //1. define fields
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="id")
+    private int id;
+
+    @Column(name="first_name")
+    private String firstName;
+
+    @Column(name="last_name")
+    private String lastName;
+
+    @Column(name="email")
+    private String email;
+
+
+
+    //2. define constructors
+
+    public Student(){}
+    public Student(String firstName, String lastName, String email){
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    //3. define getters/setters
+    //configuring this using annotation.
+
+
+
+    //4. define toString() method
+
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
