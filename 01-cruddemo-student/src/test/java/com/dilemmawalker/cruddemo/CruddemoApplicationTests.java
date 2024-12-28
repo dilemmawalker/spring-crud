6a8e83f2a1806db216f74faea7806da733ca157c
Yash Handa <yashhanda500@gmail.com>
2024-12-28 15:16:05 +0530

    added new repository

---
+++ 01-cruddemo-student/src/test/java/com/dilemmawalker/cruddemo/CruddemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilemmawalker.cruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CruddemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
