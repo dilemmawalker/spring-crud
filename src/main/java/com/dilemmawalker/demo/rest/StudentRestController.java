b65809558c162bcc4b1586a59876cd42a4b8c0af
Yash Handa <yashhanda500@gmail.com>
2025-01-04 01:47:07 +0530

    added student id controller.

--- src/main/java/com/dilemmawalker/demo/rest/StudentRestController.java
+++ src/main/java/com/dilemmawalker/demo/rest/StudentRestController.java
@@ -1,7 +1,9 @@
 package com.dilemmawalker.demo.rest;
 
 import com.dilemmawalker.demo.entity.Student;
+import jakarta.annotation.PostConstruct;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,14 +14,27 @@ import java.util.List;
 @RequestMapping("/api")
 public class StudentRestController {
 
-    @GetMapping("/students")
-    public List<Student> getStudents(){
-        List<Student> students = new ArrayList<>();
+    private List<Student> students;
+
+    @PostConstruct
+    public void loadData(){
+        students = new ArrayList<>();
 
         students.add(new Student("yash", "handa"));
         students.add(new Student("Apoorva", "gupta"));
         students.add(new Student("Iam", "existing"));
+    }
+
+
+    @GetMapping("/students")
+    public List<Student> getStudents(){
 
         return students;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId){
+
+        return students.get(studentId);
     }
 }
