4e8ca02cf012c5484c09806826694b4c8360e5b5
Yash Handa <yashhanda500@gmail.com>
2025-01-04 22:23:09 +0530

    added spring data rest

--- 04-sprin-bootcrud-employee-jpaRepository-&-spring-data-rest/src/main/java/com/dilemmawalker/yash/dao/EmployeeRepository.java
+++ 04-sprin-bootcrud-employee-jpaRepository-&-spring-data-rest/src/main/java/com/dilemmawalker/yash/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.dilemmawalker.yash.dao;
 
 import com.dilemmawalker.yash.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
+@RepositoryRestResource(path="members")//api endpoint will be exposed as /members & not as /employees
+//especially helpful in case of plural name usage.
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
     // no code needed here, JpaRespository automatically provides basic functions using class & primary key type provided.
