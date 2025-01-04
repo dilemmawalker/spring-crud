478b7cee8bd4fe517d1571e171c4e961996c0664
Yash Handa <yashhanda500@gmail.com>
2025-01-04 21:34:49 +0530

    added repo as jpa respository

---
+++ 03-sprin-bootcrud-employee-jpaRepository/src/test/java/com/dilemmawalker/yash/YashApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dilemmawalker.yash;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class YashApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
